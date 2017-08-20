29ceeebcfdc196aeb65cf6e567ce1e9a65417543
vkmb <mithun14leo@gmail.com>
2017-08-20 18:26:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+
+// move the img
+var im = document.getElementById("a4img");
+var t = document.getElementById("te");
+var l=0;
+
+function move_to_right()
+{
+l = l+1;
+im.style.marginLeft = l+'px';
+}
+
+im.onclick = function(){
+    setInterval(move_to_right, 50)
+};
+t.onclick = function(){
+    t.innerHtml = "You have opened the secret tunnel !!"
+};
