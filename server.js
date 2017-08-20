da05745b8e3051c75afd7de80ef006e1f72f6f49
vkmb <mithun14leo@gmail.com>
2017-08-20 15:58:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article_one',function(req, res){
+  res.send("On  the way ...")  
+});
+app.get('/article_two',function(req, res){
+  res.send("On  the way ...")  
+});
+app.get('/article_three',function(req, res){
+  res.send("On  the way ...")  
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
