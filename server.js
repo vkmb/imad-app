6aa528893b9f78e12c283253fda4a8265ceaaaee
vkmb <mithun14leo@gmail.com>
2017-08-20 16:43:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/article_two',function(req, res){
   res.send("On  the way ...")  
 });
 app.get('/article-four',function(req, res){
-  res.sendFile(path.join(__dirname,'ui','article_four.html'))
+  res.sendFile(path.join(__dirname,'ui','article_four.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
