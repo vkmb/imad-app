7fb030afc5091b8693c846b0915fbc29cfe8b254
vkmb <mithun14leo@gmail.com>
2017-08-26 01:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/counter',function(req, res){
 var name_list =[]
 
 app.get('/submitname', function (req, res) {
-  var name = req.qurey.v;
+  var name = req.qurey.name;
   name_list.push(name);
   res.send(JSON.stringify(name_list));
 });
