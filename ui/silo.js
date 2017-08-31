f4adbcf10eb610e6c5dfba5a9ceea15bac233800
vkmb <mithun14leo@gmail.com>
2017-08-31 15:35:23 +0530

    [imad-console] Updates ui/silo.js

--- ui/silo.js
+++ ui/silo.js
@@ -1,3 +1,4 @@
-var us = document.getElementById(username).value;
-var pa = document.getElementById(password).value;
+var us = document.getElementById('username').value;
+var pa = document.getElementById('password').value;
+var sub =  document.getElementById('sub');
 console.log(us,pa)
