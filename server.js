b84550e26f9807ef282707782b42c07dd53e54a4
vkmb <mithun14leo@gmail.com>
2017-08-31 15:00:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
-var count = 0; 
-var app = express();
-var crypto = require('crypto');
-app.use(morgan('combined'));
-var name_list =[]
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
 
+var port = 80;
+var name_list =[]
+var count = 0;
 var config  = {
     user:'mithun14leo',
     database : 'mithun14leo',
@@ -15,6 +11,22 @@ var config  = {
     port:'5432',
     password : process.env.DB_PASSWORD
 }
+// Libaries 
+
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool = require('pg').Pool;
+var app = express();
+var crypto = require('crypto');
+var bp = require('body-parser')
+var pool = new Pool(config);
+
+//
+app.use(morgan('combined'));
+app.use(bp.json());
+
+// logical
 function hash(value,salt){
     var return_value = crypto.pbkdf2Sync(value, salt, 10000, 512, 'sha512');
     return ['pbkdf', salt, '10000', return_value.toString('hex')].join('$');
@@ -52,12 +64,24 @@ function convert2html(data){
     `;
     return html_template;
 }
-var pool = new Pool(config);
-app.get('/hash/:value',function(req,res){
+
+//end pionts
+
+/*app.get('/hash/:value',function(req,res){
     var value = req.params.value;
     value = hash(value,'hello');
     res.send(value);
-});
+});*/
+/*app.get('/submit-name/:nameid', function (req, res) {
+  var name = req.params.nameid;
+  name_list.push(name);
+  res.send(JSON.stringify(name_list));
+});*/
+/*app.get('/submit-name', function (req, res) {
+  var name = req.qurey.nameid;
+  name_list.push(name);
+  res.send(JSON.stringify(name_list));
+});*/
 
 app.get('/articles/:articletitle', function (req, res) {
     pool.query("SELECT * FROM article WHERE title =  $1",[req.params.articletitle], function(err, result){
@@ -74,7 +98,20 @@ app.get('/articles/:articletitle', function (req, res) {
        }
     });
 });
-
+app.post('/create-user',function(req,res){
+    var usna = req.body.username;
+    var pass = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    pass = hash(pass, salt);
+    pool.query('INSERT INTO all_db (user_name, password) VALUES($1, $2)', [usna, pass], function(err, result){
+        if (err){
+            res.send(500).send(err.toString());
+        }
+        else{
+            res.send(200).send('User Account successfully created for '+username);
+        }
+    });
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -85,28 +122,11 @@ app.get('/counter',function(req, res){
  count = count + 1;
  res.send(count.toString());
 });
-
-
-
 app.get('/submitname', function (req, res) {
   var name = req.query.name;
   name_list.push(name);
   res.send(JSON.stringify(name_list));
 });
-/*
-app.get('/submit-name/:nameid', function (req, res) {
-  var name = req.params.nameid;
-  name_list.push(name);
-  res.send(JSON.stringify(name_list));
-});
-
-app.get('/submit-name', function (req, res) {
-  var name = req.qurey.nameid;
-  name_list.push(name);
-  res.send(JSON.stringify(name_list));
-});
-
-*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -120,11 +140,6 @@ app.get('/ui/a4img.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
