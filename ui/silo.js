8c844702749de4ee72173169634631c14ab7b195
vkmb <mithun14leo@gmail.com>
2017-08-31 22:59:34 +0530

    [imad-console] Updates ui/silo.js

--- ui/silo.js
+++ ui/silo.js
@@ -2,6 +2,7 @@ var us = document.getElementById("username");
 var pa = document.getElementById("password");
 var s =  document.getElementById("sas");
 var sub =  document.getElementById("sub");
+var log = document.getElementById("login");
 sub.onclick = function(){
 var name = us.value;
 var pass = pa.value;
@@ -27,3 +28,32 @@ console.log(pass);
     request.send(JSON.stringify({'username':name,'password':pass}));
     };
 
+log.onclick = function(){
+var name = us.value;
+var pass = pa.value;
+console.log(name);
+console.log(pass);
+        var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+            var a = request.responseText;
+            alert(a.toString());
+        }
+        else if (request.status === 500){
+            var nae_1stg = request.responseText;
+             s.innerHTML=nae_1stg.toString();
+            //alert(nae_1stg);
+        }
+        else if (request.status === 403){
+            var naes_1stg = request.responseText;
+             s.innerHTML=naes_1stg.toString();
+            //alert(nae_1stg);
+        }
+    }
+    };
+    
+    request.open("POST","http://mithun14leo.imad.hasura-app.io/create-user", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify({'username':name,'password':pass}));
+    };
