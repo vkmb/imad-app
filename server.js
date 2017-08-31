085fc1f2a20bc82a8c853b763a4365ab8e688f65
vkmb <mithun14leo@gmail.com>
2017-08-31 21:56:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/create-user',function(req,res){
             res.send(500).send(err.toString());
         }
         else{
-            res.send(200).send('User Account successfully created for '+username);
+            res.send(200);
         }
     });
 });
