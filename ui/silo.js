a131bd87742d66cb199a370b9d120f36cd63d89d
vkmb <mithun14leo@gmail.com>
2017-08-31 16:11:31 +0530

    [imad-console] Updates ui/silo.js

--- ui/silo.js
+++ ui/silo.js
@@ -4,19 +4,17 @@ var pa = document.getElementById('password');
 var sub =  document.getElementById('sub');
 sub.onClick = function(){
     console.log(us.value.toString() +"  "+ pa.value.toString());
+    var name = us.value;
+    var pass = pa.value;
     var request = new XMLHttpRequest();
     request.onreadysatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200){
                 alert("Thank you!!!")
             }
-            else {
-                
-            }
-            
         }
     }
     request.setRequestHeader('Content-Text', 'application/json');
     request.open("POST","http://mithun14leo.imad.hasura-app.io/account", true);
-    request.send(JSON.stringify({'username':us.value, 'password':pa.value}));
+    request.send(JSON.stringify({'username':name, 'password':pass}));
 }; 
