6b9bdabd6ff92e9e0403153ded27d0bb0436668b
vkmb <mithun14leo@gmail.com>
2017-09-19 19:16:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post ('/location',function(req,res){
     res.send(200);
 });
 app.get('/kaybee',function(req, res){
-    res.send(map);
+    res.send('ok');
 });
 app.post('/create-user',function(req,res){
     var usna = req.body.username;
