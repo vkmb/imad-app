43d2fe0c1568bcde746b174ae50731e47573de00
vkmb <mithun14leo@gmail.com>
2017-11-18 13:51:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -189,13 +189,13 @@ app.post('/sense', function(req, res){
 
 
 app.get('/sen_data',function(req, res){
-    pool.query('SELECT * FROM sensed_data',function(err, result){
+    pool.query('SELECT data FROM sensed_data',function(err, result){
        if (err){
            var error = err.toString();
            res.send(500).send(error);
        }
        else {
-            res.send(result.rows.data);
+            res.send(JSON.stringify(result.rows));
            
 }
            
